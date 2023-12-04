29226adc27178fa86f1bfc2863a19b3cf7cd13c0
Younes Outerbah <younesouterbah1@gmail.com>
2023-12-04 17:01:45 +0100

    finished adding users to data base and show them in the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [email, setEmail] = useState();
+
+  const dataFetching = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/users");
+      const data = res.data;
+      console.log(data);
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dataFetching();
+  }, []);
+
+  const createUser = () => {
+    axios
+      .post("http://localhost:5000/createUser", {
+        name: name,
+        age: age,
+        email: email,
+      })
+      .then((res) => console.log("user created", res.data));
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} />
+        <br />
+        <input type="number" placeholder="age" onChange={(e) => setAge(e.target.value)} />
+        <br />
+        <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <br />
+        <button onClick={createUser}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {users.map((user) => (
+        <ul key={user._id}>
+          <li>Hello my name is {user.name}</li>
+          <li>am {user.age} yo</li>
+          <li>contact me at: {user.email}</li>
+        </ul>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
