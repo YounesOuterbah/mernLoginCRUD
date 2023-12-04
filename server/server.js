c87fe7b5add5c70d845638b46ec1929453b3d012
Younes Outerbah <younesouterbah1@gmail.com>
2023-12-04 17:00:07 +0100

    adding users to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/mern-crud");
 
@@ -9,6 +13,12 @@ const UserModel = require("./models/Users");
 app.get("/users", async (req, res) => {
   const users = await UserModel.find();
   res.json(users);
+});
+
+app.post("/createUser", async (req, res) => {
+  const newUser = new UserModel(req.body);
+  await newUser.save();
+  res.json(req.body);
 });
 
 app.listen("5000", () => {
