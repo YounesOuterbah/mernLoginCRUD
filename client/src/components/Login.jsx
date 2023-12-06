99961323fed196fc1b334ead160bceb84929f6ff
Younes Outerbah <younesouterbah1@gmail.com>
2023-12-06 13:34:02 +0100

    end of project

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 
 export const Login = () => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const respones = await axios.post("http://localhost:5005/login", { username, password });
+    setCookies("access_token", respones.data.token);
+    window.localStorage.setItem("userID", respones.data.adminID);
+    console.log(respones);
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h1>LOGIN</h1>
+      <form style={{ flexDirection: "column" }} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={username}
+            name="username"
+            required
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            value={password}
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </>
+  );
+};
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export const Register = () => {
-  return <div>register</div>;
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5005/register", {
+      username,
+      password,
+    });
+    alert("Admin created");
+  };
+
+  return (
+    <>
+      <h1>REGISTER</h1>
+      <form style={{ flexDirection: "column" }} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={username}
+            name="username"
+            required
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            value={password}
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Register" />
+        </div>
+      </form>
+    </>
+  );
 };
